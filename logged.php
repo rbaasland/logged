46bc96409c064c70c4255bb773aff5481efa281f
Ron Baasland <ron.baasland@gmail.com>
2016-07-03 13:52:44 -0400

    Removed php documenter from composer Added output functions. Updated base log filename Updated log to write header/footer for each instance. Updated open to write at end of file not beginning. Added clear method Updated the write methods to write arrays, objects and strings.

--- logged.php
+++ logged.php
@@ -4,7 +4,7 @@ namespace Logged;
 
 class Logged
 {
-    private $log_file = "error.log";
+    private $log_file = "logged.log";
 
     public function setLogFile($log_file)
     {
@@ -19,18 +19,56 @@ class Logged
     public function logDataFile($line, $file, $content)
     {
         $log = $this->openLoggedFile();
-        fwrite($log, $line);
-        fwrite($log, $line);
+        $this->writeData($log, $this->writeHeading($line, $file));
+        $this->writeData($log, $content);
+        $this->writeData($log, $this->writeFooter());
         $this->closeLoggedFile($log);
     }
 
     private function openLoggedFile()
     {
-        return fopen($this->log_file, "w");
+        return fopen($this->log_file, "a");
     }
 
     private function closeLoggedFile($file)
     {
         fclose($file);
+    }
+
+    private function writeData($log, $content) {
+        if (is_array($content)) {
+            $content = $this->prepareArrayOutput($content);
+        } else{
+            $content .= PHP_EOL;
+        }
+        fwrite($log, $content);
+    }
+
+    private function writeHeading($line, $file) {
+        $date = new \DateTime();
+        $date_output = $date->format('d-m-Y h:i:s a');
+        $heading = "";
+        $heading .= "====================================================================================================\n";
+        $heading .= "Line " . $line . " on " . $file . " at " . $date_output . "\n";
+        $heading .= "====================================================================================================\n";
+        return $heading;
+    }
+
+    private function writeFooter() {
+        $footer = "\n--------------------------------------------------\n\n";
+        return $footer;
+    }    
+
+    public function clearLoggedFile() {
+        $log = fopen($this->log_file, 'w' );
+        fclose($log);
+    }
+
+    private function prepareArrayOutput($content) {
+        ob_start();
+        print_r($content);
+        $output = ob_get_contents();
+        ob_end_clean();
+        return $output;
     }
 }
